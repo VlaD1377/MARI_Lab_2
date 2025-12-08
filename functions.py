6113dedfab184ab57c43dd9ebbcd136fbbbca979
VlaD1377 <vladlabunets2003@gmail.com>
2025-12-08 10:42:07 +0200

    Corrected Dixon-Price function

--- functions.py
+++ functions.py
@@ -2,5 +2,5 @@ import numpy as np
 
 def dixon_price(x):
     x = np.asarray(x)
-    result = (x[0] - 1)**2 + sum((i + 1) * (2 * x[i]**2 - x[i-1])**2 for i in range(1, len(x)))
+    result = (x[0] - 1)**2 + sum(i * (2 * x[i]**2 - x[i-1])**2 for i in range(1, len(x)))
     return result
